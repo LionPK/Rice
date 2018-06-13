aac42bd8b4dba081a45783426eb30a5f3b7ee631
LionPK <praneed.kla@northbkk.ac.th>
2018-06-13 20:11:04 +0700

    fix db

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('LIBS', 'libs/');
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'project');
+define('DB_NAME', 'rice_area');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
--- config/database.php
+++ config/database.php
@@ -2,6 +2,6 @@
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'project');
+define('DB_NAME', 'rice_area');
 define('DB_USER', 'root');
 define('DB_PASS', '');
